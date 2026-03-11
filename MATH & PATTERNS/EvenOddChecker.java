be39c0f1cb2bb275a2ad8a9d240825c22252d173
deban-18 <debanjanmandal903@gmail.com>
2026-03-11 07:10:54 +0000

    evenodd code added

---
+++ MATH & PATTERNS/EvenOddChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        
+        if (num % 2 == 0) {
+            System.out.println("The number " + num + " is Even.");
+        } else {
+            System.out.println("The number " + num + " is Odd.");
+        }
+        
+        scanner.close(); // It's good practice to close the scanner
+    }
+}
